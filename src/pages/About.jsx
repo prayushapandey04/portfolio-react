6f388b9b39579e2f7d514315704d5545d8ae7b4a
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-10 21:55:06 -0600

    about page done

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+
+const About = () => {
+    return(
+        <div className='about-background'>
+            <Header />
+            <div className='div-container'>
+                <div className='card-section'>
+                    <div className='card-body'>
+                        <h2 className='card-title'>About Me</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
